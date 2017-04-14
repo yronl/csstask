d13cb775b4a4646b53f0db2ea24a71f737b98a84
ALittleWhile <jiban1234@163.com>
2017-04-14 17:55:26 +0800

    amend former tasks, task15 is going on

---
+++ task15/outer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by msi on 2017/4/13.
+ */
+
+var s=document.getElementById("target-ul");
+
+function showUl() {
+    if(s.style.display==""){
+        s.style.display="block";
+    }else{
+        s.style.display="";
+    }
+}
